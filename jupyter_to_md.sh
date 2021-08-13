e5bc6a8f6b108fd6f2048cf3dc53f16d610958df
erenup <ping.nie@pku.edu.cn>
2021-08-13 22:35:31 +0800

    add link

---
+++ jupyter_to_md.sh
@@ -0,0 +1,8 @@
+jm="jupyter nbconvert --to markdown"
+jm docs/篇章4-使用Transformers解决NLP任务/4.1-文本分类.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.2-序列标注.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.3-问答任务-抽取式问答.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.4-问答任务-多选问答.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.5-生成任务-语言模型.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.6-生成任务-机器翻译.ipynb
+jm docs/篇章4-使用Transformers解决NLP任务/4.7-生成任务-摘要生成.ipynb
